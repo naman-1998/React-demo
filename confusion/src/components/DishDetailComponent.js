f32c25db3e72e01b8965e568cc61f4c8956c4aba
Naman Panchal <namanpanchal17@gmail.com>
2020-06-02 11:37:58 +0530

    Commit-7 Redux Thunk

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import { Card ,CardTitle,CardImg,CardText,CardBody,Breadcrumb,BreadcrumbItem,
     Button,Modal,ModalHeader,ModalBody,Label,Row,Col}  from 'reactstrap';
 import { Link} from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 function RenderDish({dish})
@@ -74,9 +74,25 @@ function RenderDish({dish})
     }
 }
 const DishDetail= (props) =>{
-    
-    // console.log(this.props.dish);
-        if(props.dish!=null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else  if(props.dish!=null){
             return(
                 
                     <div className="container">
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { addComment } from '../redux/ActionCreators';
+import { addComment,fetchDishes } from '../redux/ActionCreators';
 const mapStateToProps = state =>{
   return{
   dishes:state.dishes,
@@ -19,26 +19,34 @@ const mapStateToProps = state =>{
 }
 const mapDishpatchToProps = dispatch =>({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId,
-     rating, author, comment))
+     rating, author, comment)),
+  fetchDishes: () => { dispatch(fetchDishes())}
 });
 class Main extends Component {
   constructor(props){
     super(props);
   
   }
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
     
   render() {
     const HomePage = ()=>{
       return(
-        <Home dish={this.props.dishes.filter((dish)=>dish.featured)[0]}
-        promotion={this.props.promotions.filter((promo)=>promo.featured)[0]}
-        leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
+        <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+               dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
+               promotion={this.props.promotions.filter((promo)=>promo.featured)[0]}
+              leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
         />
       );
     }
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess} 
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
             addComment={this.props.addComment}/>
       );
